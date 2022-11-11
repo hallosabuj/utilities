f55d5faf0d186df841c23b25fc3aa619fbd130d0
Sabuj Mondal <hallosabuj@gmail.com>
2022-11-11 12:22:30 +0530

    Little chnages

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"utilities/uri"
@@ -11,9 +13,15 @@ import (
 var router = mux.NewRouter()
 
 func main() {
+	router.HandleFunc("/greet/{name}", func(w http.ResponseWriter, r *http.Request) {
+		var vars = mux.Vars(r)
+		name := vars["name"]
+		w.Header().Add("Content-type", "application/json")
+		json.NewEncoder(w).Encode(name)
+	}).Methods("GET")
+
 	uri.Uri_init_inverter(router)
 	uri.Uri_init_calculator(router)
+	fmt.Println("Server started at port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
-	// db.ConnectDB()
-	// db.CreateInverterTable()
 }
